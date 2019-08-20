92d4a1bdb9f7735f2b233c758bbf653e88dd42b0
zjyu0724 <zjyu0724@gmail.com>
2019-08-21 07:56:15 +0800

    以Driver抓圖成功

--- MegaWeb.py
+++ MegaWeb.py
@@ -12,6 +12,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import re
 import pandas as pd
+import pickle
+import pyautogui
 
 MegaLogger.Init()
 
@@ -21,38 +23,67 @@ def GetWeb():
     soup = BeautifulSoup(r.text, 'html.parser')
     MegaLogger.Write( soup.prettify() )
 
-def GetImg( IMG_URL ):
+def GetImg( Driver, IMG_URL ):
 
+    WorkDir = os.getcwd()
 
     folder_name = "MegaPic"
     if not os.path.exists( folder_name ):
         os.makedirs( folder_name )
     today = MegaDate.GetToday()
-    folder_path = folder_name + "/" + folder_name + "-" + today + "/"
+    folder_path = folder_name + "\\" + folder_name + "-" + today + "\\"
     if not os.path.exists( folder_path ):
         os.makedirs( folder_path )
 
     # html = requests.get(item.get('src')) # use 'get' to get photo link path , requests = send request
-    img_name = folder_path + 'aimg.jpg'
+    img_name = WorkDir + '\\' + folder_path + 'aimg.jpg'
     print(img_name)
-    # urllib.request.urlretrieve(str(item.get('src')), str(img_name))
+
+
+    # Obtain session id from Selenium cookies
+    # cookies = {
+    #     'sessionhash': Driver.get_cookie('sessionhash'),
+    # }
+    # cookies = Driver.get_cookies()
+    # s = requests.session()
+    # c = requests.cookies.RequestsCookieJar()
+    # for item in cookies:
+    #     c.set(item["name"], item["value"])
+    # s.cookies.update(c)
+
+    # User agent must match user agent used to create session
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+    # }
+    # r = s.get(IMG_URL, headers=headers)
+
+    # 保存 Cookies
+    pickle.dump( Driver.get_cookies(), open("cookies.pkl", "wb"))
+
+    # 载入 Cookies
+    Driver.get(IMG_URL)
+    pyautogui.hotkey('ctrl', 's')
+    time.sleep(1)
+    pyautogui.press('shift')
+    pyautogui.typewrite(img_name)
+    pyautogui.hotkey('enter')
+    # cookies = pickle.load(open("cookies.pkl", "rb"))
+    #
+    # s = requests.Session()
+    # for cookie in cookies:
+    #     s.cookies.set(cookie['name'], cookie['value'])
+    # r = s.get(IMG_URL)
+    # bodyStr = r.text
 
     # r = requests.get(item.get('src'), stream=True, headers={'User-agent': 'Mozilla/5.0'})
-    r = requests.get(IMG_URL, stream=True, auth=('zjyu0724', 'diablo111'))
-    print("IMG_URL=" + IMG_URL)
-    print(r.status_code)
-    if r.status_code == 200:
-        with open(img_name, 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
+    # r = requests.get(IMG_URL, stream=True, auth=('zjyu0724', 'Starcraft2'))
+    # print("IMG_URL=" + IMG_URL)
+    # print(r.status_code)
+    # if r.status_code == 200:
+    #     with open(img_name, 'wb') as f:
+    #         r.raw.decode_content = True
+    #         shutil.copyfileobj(r.raw, f)
 
-    # with open(img_name,'wb') as file: #以byte的形式將圖片數據寫入
-    #
-    #     file.write(html.content)
-    #
-    #     file.flush()
-    #
-    # file.close() #close file
 
     time.sleep(1)
 # print(soup.prettify())
@@ -61,7 +92,7 @@ def LoginWeb():
 
 
     USER = 'zjyu0724'
-    PASSWORD = 'diablo111'
+    PASSWORD = 'Starcraft2'
     LOGIN_URL = 'http://www06.eyny.com/member.php?mod=logging&action=login'
 
     driver = webdriver.Chrome('./chromedriver')
@@ -74,11 +105,11 @@ def LoginWeb():
     password.send_keys(PASSWORD)
     elem.submit()
     # print(driver.page_source)
-    time.sleep(10)
+    # time.sleep(10)
     MegaLogger.Write("登入完成")
 
-    driver.get('http://www06.eyny.com/forum-205-3W0P8JX0.html')
-    # driver.get('http://www06.eyny.com/thread-12125929-1-3W0P8JX0.html')
+    # driver.get('http://www06.eyny.com/forum-205-3W0P8JX0.html')
+    driver.get('http://www06.eyny.com/thread-12125929-1-3W0P8JX0.html')
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     MegaLogger.Write(soup.prettify())
@@ -87,12 +118,12 @@ def LoginWeb():
     print( len(dfs) )
 
 
-    # imgtags = soup.find_all(id=re.compile("aimg_*"))
-    # print(imgtags[0].get('file'))
-    # GetImg(imgtags[0].get('file'))
-    # driver.close
+    imgtags = soup.find_all(id=re.compile("aimg_*"))
+    print(imgtags[0].get('file'))
+    GetImg(driver, imgtags[0].get('file'))
+    driver.close
 
-    time.sleep(20)
+    time.sleep(5)
     # session_requests = requests.session()
     # result = session_requests.get(LOGIN_URL)
     # tree = html.fromstring(result.text)
